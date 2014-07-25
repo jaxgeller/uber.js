6ce4238fe63d0ace3336e566daf3ae42e208c2a4
jacksongeller <jacksongeller@gmail.com>
2014-07-24 21:01:34 -0400

    refactoring

--- client/index.js
+++ client/index.js
@@ -2,18 +2,34 @@ var keys = require('./keys');
 var request = require('request');
 
 function UberClient (token, deviceId) {
-  this.opts = {
-    token: keys.token,
-    deviceId: keys.deviceId,
-    app: 'client'
+  this.profile = {
+    token: token,
+    deviceId: deviceId,
+    app: 'client',
+    uri: 'https://cn-dc1.uber.com/'
   }
-  this.request = {
+  this.location = {
+    longitude:
+    latitude:
+    setLocation: function(long, lat) {
+      this.location.longitude = long;
+      this.location.lat = lat;
+    }
+  }
+  this.token = token;
+  this.deviceId = deviceId;
+  this.app = 'client';
+  this.uri = 'https://cn-dc1.uber.com/';
+}
+
+
+
+
+this.request = {
     method: 'POST',
     uri: keys.uri,
     json: this.opts
   }
-}
-
 var uber = new UberClient(keys.token, keys.deviceId);
 
 uber.pingClient('----','----', function(res) {
